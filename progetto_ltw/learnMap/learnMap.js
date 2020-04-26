6205e1130c15361d58551427cbd3acca9f9c5a28
Simone <rotondi97simone@gmail.com>
2020-04-26 23:54:15 +0200

    test LearnMap

--- progetto_ltw/learnMap/learnMap.js
+++ progetto_ltw/learnMap/learnMap.js
@@ -1,3 +1,16 @@
 /** @format */
 
-function Abilita(e) {}
+function decolora(e) {
+	e.target.style.color = "initial";
+}
+function coloraBianco(e) {
+	e.target.style.color = "white";
+}
+
+function assegnaEventHandlers() {
+	var c1Doc = document.getElementsByTagName("a");
+	for (i = 0; i < c1Doc.length; i++) {
+		c1Doc[i].addEventListener("mouseenter", coloraBianco);
+		c1Doc[i].addEventListener("mouseleave", decolora);
+	}
+}
