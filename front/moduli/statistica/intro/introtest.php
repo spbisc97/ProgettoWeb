33120dc0d5da500965cf89019e09f4e23c5ded1f
Simone <rotondi97simone@gmail.com>
2020-05-10 15:34:23 +0200

    introtest

--- front/moduli/statistica/intro/introtest.php
+++ front/moduli/statistica/intro/introtest.php
@@ -1,6 +1,52 @@
-<?php
-/**
- * @format
- */
+<!DOCTYPE html>
+<html lang="en">
 
-echo "hello world";
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link rel="stylesheet" href="../../../bootstrap-4.4.1-dist/css/bootstrap.css" />
+        <link rel="stylesheet" href="../../../assets/css/home-made.css" />
+        <script src="../../../jQuery/jquery.min.js"></script>
+        <script src="../../../assets/js/bootstrap.js" lang="--js"></script>
+    </head>
+
+    <body>
+        <nav>
+
+            <body>
+                <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
+                    <!-- href="#" è un ancora in realtà a nulla ma ci permettere di usare il js
+			le classi navbar-light/dark e bg-light/dark sono per il tema il primo per
+			uso a invece che span per rendere il contenuto interattivo  -->
+                    <a class="navbar-brand" href="#">Edu@home</a>
+                    <!-- ancora come in questo caso -->
+                    <button class="navbar-toggler" type="button" data-toggle="collapse"
+                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <!-- sopra questo abbiamo una parte che resterà comunque è formattata la pagina, 
+                mentre sotto abbiamo una parte collapsable ovvero 
+                che formerà un menù ad hamburger (grazie alle magie js)nel caso la pagina fosse troppo stretta per mostrare tuto -->
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item active">
+                                <a id="home" class="nav-link" href="../../../learnMap/learnMap.html">Home <span
+                                        class="sr-only">(current)</span></a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+                <br><br><br>
+                <div>
+                    <?php
+                    echo "complimenti!! <br>";
+                    if (true) {
+                    	echo "o forse no";
+                    }
+                    ?>
+                </div>
+            </body>
+
+            <html>
