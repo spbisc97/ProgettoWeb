512df9712d9c9e509c11bac835735ca07989db66
giovannino <orlandogiovannino@gmail.com>
2020-05-13 02:31:21 +0200

    continuo db

--- front/paginaLogin/checkLogin.php
+++ front/paginaLogin/checkLogin.php
@@ -7,25 +7,26 @@
     ($dbconn = pg_connect(
         "host=localhost port=5433 dbname=Edu@homedb user=postgres password=pippopluto"
     )) or die("Could not connect: " . preg_last_error());
-    if (!isset($_POST['Submit'])) {
+    if (!isset($_POST['Login'])) {
         header("Location: ../index.html");
     } else {
-        $email = $_POST["email"];
-        $q1 = "select * from utente where email=$1";
+        $email = $_POST['email'];
+        $q1 = "select * from utente where email= $1";
         $result = pg_query_params($dbconn, $q1, [$email]);
         if ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
             echo "<h1>non hai un account registrato<h1>
                             <a href=../paginaRegistrazione/registrazione.html> clicca qui per registrarti</a>";
-        } else {
+        }
+        else {
             $password = md5($_POST["password"]);
-            $q2 = "select* from utente where email =$1 and password = $2";
+            $q2 = "select * from utente where email =$1 and password = $2";
             $result = pg_query_params($dbconn, $q2, [$email, $password]);
             if (!($line = pg_fetch_array($result, null, PGSQL_ASSOC))) {
                 echo "<h1> La password è sbagliata</h1>
                     <a href=login.html> clicca qui per il login<h/a>";
             } else {
                 $nome = $line["nome"];
-                echo "<a href=../index.html> Premi qui</a>";
+                echo "<a href=../learnMap/learnMap.php> Premi qui</a>";
             }
         }
     }
--- front/paginaRegistrazione/validateRegistrazione.php
+++ front/paginaRegistrazione/validateRegistrazione.php
@@ -5,24 +5,16 @@
 <body>
     <?php
     ($dbconn = pg_connect(
-<<<<<<< HEAD
         "host=localhost port=5433 dbname=Edu@homedb user=postgres password=pippopluto"
-=======
-        "host=localhost port=5433 dbname=Edu@homedb user=postgres password=password"
->>>>>>> 05598aed8cdd385a551abb8b9124009a2a6fbc0e
     )) or die("Could not connect: " . preg_last_error());
     if (!isset($_POST['Submit'])) {
         header("Location: ../index.html");
     } else {
-        $Email = $_POST['email'];
-<<<<<<< HEAD
-        $q1 = "select * from utente where Email= $1";
-=======
-        $q1 = "select * from Utent where Email= $1";
->>>>>>> 05598aed8cdd385a551abb8b9124009a2a6fbc0e
+        $email = $_POST['email'];
+        $q1 = "select * from utente where email= $1";
         $result = pg_query_params($dbconn, $q1, [$email]);
         if ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
-            echo "<h1>Sorry you are already a registered user<h1>
+            echo "<h1>hai già un utente registrato<h1>
 						<a href=../paginaLogin/login.html> Click here to login</a>";
         } else {
             $Nick = $_POST["nick"];
@@ -30,11 +22,7 @@
             $Nome = $_POST["nome"];
             $Cognome = $_POST["cognome"];
             $Password = md5($_POST['password']);
-<<<<<<< HEAD
             $q2 = "insert into utente (nick,email,nome,cognome,password) values($1,$2,$3,$4,$5)";
-=======
-            $q2 = "insert into Utent values($1,$2,$3,$4,$5)";
->>>>>>> 05598aed8cdd385a551abb8b9124009a2a6fbc0e
             $data = pg_query_params($dbconn, $q2, [$Nick, $Email, $Nome, $Cognome, $Password]);
             if ($data) {
                 //header("Location: registrationCompleted.html);
@@ -46,9 +34,6 @@
     }
     ?>
 </body>
-<<<<<<< HEAD
-Nome:
-=======
->>>>>>> 05598aed8cdd385a551abb8b9124009a2a6fbc0e
+<<<<<<< HEAD Nome:=======>>>>>>> 05598aed8cdd385a551abb8b9124009a2a6fbc0e
 
 </html>
