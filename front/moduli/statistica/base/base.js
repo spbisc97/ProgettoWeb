73fb4cf2a022522d5b71881b3e293315a15ecb65
Simone <rotondi97simone@gmail.com>
2020-05-17 01:56:45 +0200

    graph!

--- front/moduli/statistica/base/base.js
+++ front/moduli/statistica/base/base.js
@@ -1,5 +1,6 @@
 /** @format */
-valori: [];
+
+media = 10;
 $(document).ready(
 	//alert("pagina caricata"),
 	//$("#sfen").show("slow"),
@@ -12,13 +13,64 @@ $(document).ready(
 		})
 );
 
+let myChart = $("#myChart");
+Chart.defaults.global.defaultFontSize = 18;
+Chart.defaults.global.defaultFontColor = "rgb(255,255,255)";
+
+let mediaChart = new Chart(myChart, {
+	type: "bar",
+	data: {
+		labels: [],
+		datasets: [
+			{
+				//label: "Media",
+				data: [],
+				backgroundColor: "rgba(200, 100, 100, 0.9)",
+				borderWidth: 1,
+				borderColor: "rgb(200,200,200)",
+				hoverBorderWidth: 1,
+			},
+			{
+				type: "line",
+				data: [],
+				backgroundColor: "rgba(100, 200, 200, 0.7)",
+				borderWidth: 3,
+				borderColor: "rgb(0,0,0)",
+				hoverBorderWidth: 3,
+			},
+		],
+	},
+	options: {
+		title: {
+			display: true,
+			text: "La Media",
+			fontSize: 25,
+		},
+		legend: {
+			display: false,
+			position: "right",
+			labels: {},
+		},
+		scales: {
+			yAxes: {
+				gridLines: {
+					zeroLineColor: "white",
+					zeroLineWidth: 5,
+				},
+				ticks: {
+					min: 0,
+				},
+			},
+		},
+	},
+});
+
 var app = new Vue({
 	el: "#app",
 	data: {
 		media: 0,
 		numer: 0,
 		totale: 0,
-		valori: [],
 		nuovovalore: 0,
 	},
 	methods: {
@@ -26,19 +78,11 @@ var app = new Vue({
 			this.numer++;
 			this.totale = this.totale + this.nuovovalore;
 			this.media = this.totale / this.numer;
-			this.valori.push(this.nuovovalore);
+			media = this.media;
+			mediaChart.data.datasets[0].data.push(this.nuovovalore);
+			mediaChart.data.labels.push(this.numer);
+			mediaChart.data.datasets[1].data.push(this.media);
+			mediaChart.update();
 		},
 	},
 });
-var options = {
-	chart: {
-		type: "bar",
-	},
-	series: [
-		{
-			//	name: 'sales'
-			data: app.valori,
-		},
-	],
-};
-var chart = new ApexCharts(document.querySelector("#chart"), options);
