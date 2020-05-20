efa527003593876213b6d1db4ca3830ebc2f9e5c
Simone <rotondi97simone@gmail.com>
2020-05-20 16:11:59 +0200

    reg prova

--- front/moduli/statistica/intro/introtest.php
+++ front/moduli/statistica/intro/introtest.php
@@ -80,7 +80,9 @@
                     	"host=dumbo.db.elephantsql.com
                     port=5432 dbname=putfahxs user=putfahxs password=yj6L-sA-nVPEpp4PJPpjvHLb6KbZJXsL"
                     )) or die("Could not connect: " . preg_last_error());
+
                     $q1 = "UPDATE statistica SET esericizio1=$1 WHERE id = $2";
+
                     $res = pg_query_params($dbconn, $q1, [$punteggio, $_SESSION["id"]]);
 
                     if ($punteggio > 2) {
