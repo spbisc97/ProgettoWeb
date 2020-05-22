2c2f35983ee35b0d731c0e3c02bc142fbfb8fd2d
Francesco Riccioni <francescoriccioni.fr@gmail.com>
2020-05-22 11:36:46 +0200

    punteggio risposte quiz

---
+++ front/moduli/qi/quiz_qi/quiz.js
@@ -0,0 +1,37 @@
+/** @format */
+
+$(document).ready(function () {
+	$(".container").click(function () {
+		$(this).parent().children(".active").removeClass("active");
+		$(this).parent().children(".active").text("ciao");
+		$(this).addClass("active");
+	});
+});
+
+
+function invia() {
+	var dom1 = $("#quiz1 .active").attr("id");
+	var dom2 = $("#quiz2 .active").attr("id");
+    var dom3 = $("#quiz3 .active").attr("id");
+    var dom4 = $("#quiz4 .active").attr("id");
+    var dom5 = $("#quiz5 .active").attr("id");
+    
+	if (!dom1 || !dom2 || !dom3 || !dom4 || !dom5) {
+		alert("completa le risposte!");
+		return false;
+	}
+	$.post("quiz.php", { prima: dom1, seconda: dom2, terza: dom3, quarta: dom4, quinta: dom5 }, function (data, status) {
+		//alert("Data: " + data + "\nStatus: " + status);
+		if (data == -1) {
+			$("#punteggio").text(
+				"Mi dispiace ma non risulti loggato e non potrai partecipare alle classifiche globali"
+			);
+		} else if (data <= 1) {
+			$("#punteggio").css("background-color", "rgba(240, 20, 30, 0.5)");
+			$("#punteggio").text("hai risposto in modo corretto a " + data + " domande su 5");
+		} else {
+			$("#punteggio").css("background-color", "rgba(20, 230, 30, 0.5)");
+			$("#punteggio").text("hai risposto in modo corretto a " + data + " domande su 5");
+		}
+	});
+}
--- front/moduli/qi/quiz_qi/quiz.php
+++ front/moduli/qi/quiz_qi/quiz.php
@@ -2,21 +2,22 @@
 /**
  * @format
  */
+session_start();
 $punteggio = 0;
 
-if ($_POST["arrayrisposte1"][0] == "d1.3" ) {
+if ($_POST["prima"] == "1.3" ) {
 	$punteggio += 1;
 }
-if ($_POST["arrayrisposte1"][1] == "d2.2") {
+if ($_POST["seconda"] == "2.2") {
 	$punteggio += 1;
 }
-if ($_POST["arrayrisposte1"][2] == "d3.4") {
+if ($_POST["terza"] == "3.4") {
 	$punteggio += 1;
 }
-if ($_POST["arrayrisposte1"][3] == "d4.2") {
+if ($_POST["quarta"] == "4.2") {
 	$punteggio += 1;
 }
-if ($_POST["arrayrisposte1"][4] == "d5.4") {
+if ($_POST["quinta"] == "5.4") {
 	$punteggio += 1;
 }
 
