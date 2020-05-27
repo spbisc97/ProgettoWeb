4bc54d8f2c3dd1ecee13dcbe5bb2c5e9e3e80a59
Francesco Riccioni <francescoriccioni.fr@gmail.com>
2020-05-27 16:26:56 +0200

    push

--- front/moduli/qi/quiz_qi/quiz.php
+++ front/moduli/qi/quiz_qi/quiz.php
@@ -24,4 +24,12 @@ if (!isset($_SESSION["id"])) {
     $punteggio = -1;
 }
 
+
+($dbconn = pg_connect(
+    "host=dumbo.db.elephantsql.com
+port=5432 dbname=putfahxs user=putfahxs password=yj6L-sA-nVPEpp4PJPpjvHLb6KbZJXsL"
+)) or die("Could not connect: " . preg_last_error());
+$q1 = "UPDATE logica SET esercizio1=$1 WHERE id = $2";
+$res = pg_query_params($dbconn, $q1, [$punteggio, $_SESSION["id"]]);
+
 echo $punteggio;
