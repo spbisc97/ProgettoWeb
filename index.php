d3a731b87788debf5deea60961d40e5745fe894b
Simone <rotondi97simone@gmail.com>
2020-05-27 19:26:28 +0200

    index-iniziale

--- index.php
+++ index.php
@@ -2,19 +2,20 @@
 
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<meta charset="UTF-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<meta http-equiv="refresh" content="15;URL=/front/index.php">
-		<title>Pagina di Redirect</title>
-	</head>
-	<body>
-	<p>
-    Tra 15 secondi avverrà un redirect automatico alla nuova pagina.<br>
-    Se non vuoi aspettare <a href="/front/index.php">clicca qui</a>.
-    </p>
-	
-	</body>
+
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="refresh" content="2;URL=/front/index.php">
+        <title>Pagina di Redirect</title>
+    </head>
+
+    <body>
+        <p>
+            Tra pochi secondi avverrà un redirect automatico alla nuova pagina.<br>
+            Se non vuoi aspettare <a href="/front/index.php">clicca qui</a>.
+        </p>
+
+    </body>
+
 </html>
-
-
